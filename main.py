f724fea18d2330ac2ad89419f1aa68e950e3c059
Tosakin68 <120755895+Tosakin68@users.noreply.github.com>
2022-12-31 10:58:13 +0100

    Added category combobox

--- main.py
+++ main.py
@@ -9,17 +9,44 @@ with open("themes.json", "r",encoding="utf-8") as f:
   themes_json = json.load(f)
 
 # Konvertiere das JSON-Objekt in eine Liste von Themen
-themes = themes_json["themes"]
+gaming = themes_json["gaming"]
+movies = themes_json["movies"]
+music = themes_json["music"]
 
 # Funktion zum Generieren eines zufälligen Themas
 def generate_theme():
-  theme_label.config(text=random.choice(themes))
+  theme_label.config(text=random.choice(music))
 
 # Erstelle das Hauptfenster
 window = Tk()
 window.title("Topic-Generator")
 window.iconbitmap("icon.ico")
 window.minsize(600, 400)
+
+# Erstelle die Kombobox für die Themenbereiche
+category_var = StringVar(window)
+category_var.set("all") # Standardauswahl
+category_dropdown = OptionMenu(window, category_var, "all", "gaming", "movies", "music")
+category_dropdown.pack()
+
+def update_themes(category):
+
+  # Aktualisiere das Label mit einem zufälligen Thema
+  if(category == "all"):
+    choices = [gaming, movies, music]
+    random_string = random.choice(choices)
+    theme_label.config(text=random.choice(random_string))
+  else:
+    if(category == "gaming"):
+        category = gaming
+    if(category == "movies"):
+        category = movies
+    if (category == "music"):
+        category = music
+
+    theme_label.config(text=random.choice(category))
+
+category_var.trace("w", lambda *args: update_themes(category_var.get()))
 
 # Erstelle ein Label zur Anzeige des generierten Themas
 theme_label = Label(window, text="")
@@ -30,6 +57,8 @@ theme_label.place(relx=0.5, rely=0.5, anchor="center")
 generate_button = Button(window, text="Generate Topic", command=generate_theme)
 generate_button.pack()
 generate_button.place(relx=0.5, rely=0.6, anchor="center")
+
+generate_button.config(command=lambda: update_themes(category_var.get()))
 
 # Starte die GUI-Schleife
 window.mainloop()
